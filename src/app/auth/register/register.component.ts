fb2daa1931021f162f5a54f7105a5b87f08b3697
Gustavo Chavez <gchavez@bancoripley.com>
2023-03-03 14:50:23 -0300

    correcciones

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  public registerForm = this.fb.group({
+    nombre: ['Gustavo', [Validators.required]],
+    email: ['test100@gmail.com', [Validators.required]],
+    password: ['123456', [Validators.required]],
+    password2: ['123456', [Validators.required]],
+    terminos: [true, [Validators.required]],
+  });
 
+  constructor( private fb: FormBuilder){
+
+  }
+  crearUsuario(){
+    console.log(this.registerForm.value);
+  }
 }
